f16fae673241b109cb51ca854c5357c67dc77b9f
Mehrshad <mehrshadsalahi1000@gmail.com>
2025-05-16 17:54:19 +0330

    upgrades added as items in blacksmith

--- models/Shops/Blacksmith.java
+++ models/Shops/Blacksmith.java
@@ -22,6 +22,26 @@ public class Blacksmith {
         returnvalue.add(new Item("Iron Ore", 393, 150, 0, 1000000));
         returnvalue.add(new Item("Gold Ore", 394, 400, 0, 1000000));
         returnvalue.add(new Item("Coal", 396, 150, 0, 1000000));
+        returnvalue.add(new Item("Copper Pickaxe" ,1000,2000,1000,1));
+        returnvalue.add(new Item("Steel Pickaxe" ,1001,5000,1000,1));
+        returnvalue.add(new Item("Gold Pickaxe" ,1002,10000,1000,1));
+        returnvalue.add(new Item("Iridium Pickaxe" ,1003,25000,1000,1));
+        returnvalue.add(new Item("Copper Axe" ,1004,2000,1000,1));
+        returnvalue.add(new Item("Steel Axe" ,1005,5000,1000,1));
+        returnvalue.add(new Item("Gold Axe" ,1006,10000,1000,1));
+        returnvalue.add(new Item("Iridium Axe" ,1007,25000,1000,1));
+        returnvalue.add(new Item("Copper WateringCan" ,1008,2000,1000,1));
+        returnvalue.add(new Item("Steel WateringCan" ,1009,5000,1000,1));
+        returnvalue.add(new Item("Gold WateringCan" ,1010,10000,1000,1));
+        returnvalue.add(new Item("Iridium WateringCan" ,1011,25000,1000,1));
+        returnvalue.add(new Item("Copper Hoe" ,1012,2000,1000,1));
+        returnvalue.add(new Item("Steel Hoe" ,1013,5000,1000,1));
+        returnvalue.add(new Item("Gold Hoe" ,1014,10000,1000,1));
+        returnvalue.add(new Item("Iridium Hoe" ,1015,25000,1000,1));
+        returnvalue.add(new Item("Copper Trash Can" ,1016,2000,1000,1));
+        returnvalue.add(new Item("Steel Trash Can" ,1017,5000,1000,1));
+        returnvalue.add(new Item("Gold Trash Can" ,1018,10000,1000,1));
+        returnvalue.add(new Item("Iridium Trash Can" ,1019,25000,1000,1));
         return returnvalue;
     }
 
