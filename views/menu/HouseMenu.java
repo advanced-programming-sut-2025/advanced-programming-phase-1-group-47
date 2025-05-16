76164623dbbe1ca7cf92068faa0d845810c09f87
parsa Sarsar <parsasrsryalanda@gmail.com>
2025-05-16 17:48:33 +0330

    new shit

--- views/menu/HouseMenu.java
+++ views/menu/HouseMenu.java
@@ -1,6 +1,9 @@
 package views.menu;
 
 import controllers.HouseMenuController;
+import models.App;
+import models.enums.Menu;
+import models.enums.commands.GameMenu;
 
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -15,12 +18,22 @@ public class HouseMenu extends AppMenu {
         Matcher matcher;
         if ((matcher = models.enums.commands.GameMenu.cookingrefrigeratorput.getMatcher(input)) != null) {
             // handleCookingRefrigeratorPut(matcher);
-        } else if ((matcher = models.enums.commands.GameMenu.cookingrefrigeratorpick.getMatcher(input)) != null) {
+        }
+        else if ((matcher = models.enums.commands.GameMenu.cookingrefrigeratorpick.getMatcher(input)) != null) {
             // handleCookingRefrigeratorPick(matcher);
-        } else if ((matcher = models.enums.commands.GameMenu.cookingreciepe.getMatcher(input)) != null) {
+        }
+        else if ((matcher = models.enums.commands.GameMenu.cookingreciepe.getMatcher(input)) != null) {
             // handleCookingRecipe(matcher);
-        } else if ((matcher = models.enums.commands.GameMenu.cookingprepare.getMatcher(input)) != null) {
+        }
+        else if ((matcher = models.enums.commands.GameMenu.cookingprepare.getMatcher(input)) != null) {
             // handleCookingPrepare(matcher);
         }
+        else if ((matcher = GameMenu.Back.getMatcher(input)) != null) {
+            App.currentMenu = Menu.GameMenu;
+        }
+        else
+            System.out.println("Invalid Command");
+    }
+
     }
 }
