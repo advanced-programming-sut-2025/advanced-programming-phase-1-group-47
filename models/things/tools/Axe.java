6cd2cebe333a1b6139850bc21125f464ae8fe5ac
AmooJoey <ahmadrezat94@gmail.com>
2025-05-17 19:28:33 +0330

    tools energy

--- models/things/tools/Axe.java
+++ models/things/tools/Axe.java
@@ -26,7 +26,7 @@ public class Axe extends Item {
             fraction++;
         }
 
-        if(App.getCurrentGame().getCurrentPlayer().getBuff().getType().equals(SkillType.FORAGING)) {
+        if(App.getCurrentGame().getCurrentPlayer().getBuff() != null && App.getCurrentGame().getCurrentPlayer().getBuff().getType().equals(SkillType.FORAGING)) {
             fraction++;
         }
         return (int) (type.getEnergyCost() * App.getCurrentGame().getWeather().getIntensity() - fraction);
