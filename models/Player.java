80b813380be2cf3602834ea03f129652e8d93574
parsa Sarsar <parsasrsryalanda@gmail.com>
2025-05-17 14:40:34 +0330

    Dont Khow!

--- models/Player.java
+++ models/Player.java
@@ -73,6 +73,16 @@ public class Player extends User {
         invetory.addItem(new FishingPole(RodType.TRAININGROD));
         invetory.addItem(new Scythe());
         invetory.addItem(new Item("Carrot Seeds", 403, 2, 0, 1000));
+        invetory.addItem(new Building(FarmBuilding.BARN));
+        invetory.addItem(new Building(FarmBuilding.BIG_BARN));
+        invetory.addItem(new Building(FarmBuilding.COOP));
+        invetory.addItem(new Building(FarmBuilding.BIG_COOP));
+        invetory.addItem(new Building(FarmBuilding.DELUXE_BARN));
+        invetory.addItem(new Building(FarmBuilding.DELUXE_BARN));
+        invetory.addItem(new Building(FarmBuilding.SHIPPING_BIN));
+        invetory.addItem(new Building(FarmBuilding.SHIPPING_BIN));
+        invetory.addItem(new Building(FarmBuilding.WELL));
+
 //        Item x = AllTheItemsInTheGame.getItemById(338);
 //        x.setAmount(45);
 //        invetory.addItem(x);
